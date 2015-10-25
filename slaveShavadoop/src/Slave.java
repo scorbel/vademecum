cac2c4a27bae30291e73f2de400a6c994a5c5613
Sophie <sophie.corbel@laposte.net>
2015-10-25 21:03:35 +0100

    Modif du 19/10

--- slaveShavadoop/src/Slave.java
+++ slaveShavadoop/src/Slave.java
@@ -1,51 +1,80 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.StringTokenizer;
 
 import data.MappedData;
-
+import data.MappedDataList;
 
 public class Slave {
 	private String name;
-	
-	public static String commonDir = "data";
-	static public String getPathFile()
-	{
-		return System.getProperty("user.dir");
 
-	}
-	public String getOutputFilePath(String id) {
-		return  getPathFile()+"/../"+commonDir+"/"+name+".txt";
+	enum Task {
+		SX, UMX, SMX
+	};
+
+	public Slave() {
+		// name = InetAddress.getLocalHost().getHostName();
 	}
 
-	
-	public Slave() throws UnknownHostException {
-		name = InetAddress.getLocalHost().getHostName();
-	}
-
-	private void splitBlockInFile(String block, String id) throws IOException {
-		PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter(getOutputFilePath(id))));
-		StringTokenizer tokenizer = new StringTokenizer(block);
-		while (tokenizer.hasMoreElements()) {
-			MappedData data = new MappedData((String) tokenizer.nextElement());
+	private MappedDataList splitBlockFromFile(String filename)
+			throws IOException {
+		BufferedReader reader = new BufferedReader(new FileReader(filename));
+		String line;
+		MappedDataList dataList = new MappedDataList();
+		while ((line = reader.readLine()) != null) {
+			String block = line.trim();
+			dataList.addLine(block);
 		}
+		reader.close();
+		return dataList;
 	}
+
+	private void sxToUmx(String filename) throws IOException {
+		MappedDataList dataList = splitBlockFromFile(MappedData.getDataDir()
+				+ "/" + filename);
+		System.out.print(dataList.toString());
+
+	}
+
+	private void testSleep() throws InterruptedException {
+		System.out.println("Avant sleep");
+		Thread.sleep(10000);
+		System.out.println("Après sleep");
+	}
+
+	public static void printUsage() {
+		String message = "Slave.jar [";
+		String sep = "";
+		for (Task task : Task.values()) {
+			message += sep + task.toString();
+			sep = "|";
+		}
+		message += "<filename>";
+		System.err.println("Illegal arguments, Usage " + message);
+	}
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		try {
-			System.out.println("Avant sleep");
-			Thread.sleep(10000);
-			System.out.println("Après sleep");
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (args.length < 2) {
+			printUsage();
+		} else {
+			String filename = args[1];
+			Slave slave = new Slave();
+			switch (args[0]) {
+			case "SX":
+				try {
+					slave.sxToUmx(filename);
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.err.println("SX TO UMX failed");
+				}
+				break;
+			default:
+				printUsage();
+
+			}
 		}
 
 	}
