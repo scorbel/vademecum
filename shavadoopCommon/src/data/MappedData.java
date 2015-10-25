a025bb2a2e3a207a42692e9ff0252caced8c7a2d
Sophie <sophie.corbel@laposte.net>
2015-10-25 21:02:55 +0100

    Modif du 19/10

--- shavadoopCommon/src/data/MappedData.java
+++ shavadoopCommon/src/data/MappedData.java
@@ -1,37 +1,48 @@
 package data;
-import java.util.Collections;
-
 
 public class MappedData implements Comparable<MappedData> {
 
+	private static String DATA = "data";
+
+	public static String getDataDir() {
+		return System.getProperty("user.home") + "/" + DATA;
+	}
+
 	private String mot = new String();
 	private Integer counter = 1;
+
 	public MappedData(String mot, Integer counter) {
 		this.setMot(mot);
 		this.setCounter(counter);
 	}
+
 	public MappedData(String mot) {
 		this.setMot(mot);
 	}
+
 	public String getMot() {
 		return mot;
 	}
+
 	public void setMot(String mot) {
 		this.mot = mot;
 	}
+
 	public Integer getCounter() {
 		return counter;
 	}
+
 	public void setCounter(Integer counter) {
 		this.counter = counter;
 	}
+
 	@Override
 	public int compareTo(MappedData other) {
 		return this.mot.compareTo(other.mot);
 	}
-	
+
 	public String toString() {
-		return mot+","+counter;
+		return mot + "," + counter;
 	}
 
 }
--- shavadoopCommon/src/data/MappedDataList.java
+++ shavadoopCommon/src/data/MappedDataList.java
@@ -1,22 +1,34 @@
 package data;
+
 import java.util.ArrayList;
 import java.util.Collections;
-
-
 
 public class MappedDataList {
 	public static String Separator = " ";
 	private ArrayList<MappedData> data = new ArrayList<MappedData>();
-	public MappedDataList(String line) {
+
+	public MappedDataList() {
+	}
+
+	public void addLine(String line) {
 		String tokens[] = line.split(Separator);
-		for ( String token : tokens) {
+		for (String token : tokens) {
 			data.add(new MappedData(token));
 		}
 	}
-	
+
 	public void sort() {
 		Collections.sort(data);
 	}
-	
+
+	public String toString() {
+		String result = "";
+		String sep = "";
+		for (MappedData mdata : data) {
+			result += sep + mdata.toString();
+			sep = "\n";
+		}
+		return result;
+	}
 
 }
